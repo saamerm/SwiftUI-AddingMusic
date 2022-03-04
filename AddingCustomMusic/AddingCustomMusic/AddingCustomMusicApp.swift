e2292fa2d986312e01c94926caadf9a80389e9c9
Saamer Mansoor <i@saamer.me>
2022-03-04 15:42:52 -0500

    BlankTemplate

---
+++ AddingCustomMusic/AddingCustomMusic/AddingCustomMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingCustomMusicApp.swift
+//  AddingCustomMusic
+//
+//  Created by Saamer Mansoor on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AddingCustomMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
