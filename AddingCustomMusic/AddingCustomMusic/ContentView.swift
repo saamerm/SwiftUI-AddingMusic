570be8d24f663d430416ae70484484aa6179d0c5
Saamer Mansoor <i@saamer.me>
2022-03-04 15:47:49 -0500

    Basic Playing sound

--- AddingCustomMusic/AddingCustomMusic/ContentView.swift
+++ AddingCustomMusic/AddingCustomMusic/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .onAppear{
+                playSound(sound: "pmSong", type: "mp3")
+            }
             .padding()
     }
 }
